1b63a6ea03e2fd3b798e2804cda0d4ec78f60280
Preston Hager <preston@hagerfamily.com>
2025-02-28 17:23:18 -0700

    Added delay safety feature turning off relays

--- PropulsionLauncherReceiver/PropulsionLauncherReceiver.ino
+++ PropulsionLauncherReceiver/PropulsionLauncherReceiver.ino
@@ -1,13 +1,23 @@
 
 #include "heltec.h"
+// pins for relays
 int relay1 = 12;
 int relay2 = 13;
 int relay3 = 2;
 int relay4 = 17;
-int interval = 70;
 
+// time activated for the safety shutoff feature
+int relay1Time = -1;
+int relay2Time = -1;
+int relay3Time = -1;
+int relay4Time = -1;
 
-#define BAND    915E6  //you can set band here directly,e.g. 868E6,915E6
+// number of miliseconds allowed to pass before auto-shutoff
+#define SAFETY_TIME1  3000
+#define SAFETY_TIME2  3000
+#define SAFETY_TIME3  3000
+#define SAFETY_TIME4  3000
+#define BAND          915E6  //you can set band here directly,e.g. 868E6,915E6
 
 void setup() {
   //  WIFI Kit series V1 not support Vext control
@@ -28,7 +38,6 @@ void setup() {
 
   pinMode(relay4, OUTPUT);
   digitalWrite(relay4, LOW);
-
 }
 
 void loop() {
@@ -36,44 +45,37 @@ void loop() {
   int packetSize = LoRa.parsePacket();
 
   if (packetSize) {
-
     // received a packet
-    //Serial.println("Received packet '");
-    // read packet
+    // read the packet
     while (LoRa.available()) {
-
-      //      Heltec.display->drawString(0, 0, "Receiving");
-      //      Heltec.display->display();
-
       String inmessage =  LoRa.readString();
       inmessage.remove(3, 1);
-      //Serial.println(inmessage);
-
 
       if (inmessage == "111") {
+        relay1Time = millis();
         digitalWrite(relay1, HIGH);
         //delay(150);
         Serial.println("relay1on");
       }
       if (inmessage == "222") {
+        relay2Time = millis();
         digitalWrite(relay2, HIGH);
         //delay(150);
         Serial.println("relay2on");
       }
 
       if (inmessage == "333") {
+        relay3Time = millis();
         digitalWrite(relay3, HIGH);
         //delay(150);
         Serial.println("relay3on");
       }
       if (inmessage == "444") {
+        relay4Time = millis();
         digitalWrite(relay4, HIGH);
         // delay(150);
         Serial.println("relay4on");
       }
-
-
-
 
       if (inmessage == "101") {
         digitalWrite(relay1, LOW);
@@ -96,31 +98,36 @@ void loop() {
         // delay(150);
         Serial.println("relay4off");
       }
-
-
-
-//      // print RSSI of packet
-//      Serial.println("' with RSSI ");
-//      Serial.printlnln(LoRa.packetRssi());
-
-
     }
-
-
-
-
-
   }
 
-
-  //  digitalWrite(relay1, LOW);
-  //  digitalWrite(relay2, LOW);
-  //  digitalWrite(relay3, LOW);
-  //  digitalWrite(relay4, LOW);
-
-
-
-
-
-
+  // ensure relays don't stay on for too long
+  if (relay1Time > 0) {
+    if (millis() - relay1Time > SAFETY_TIME1) {
+      digitalWrite(relay1, LOW);
+      relay1Time = -1;
+      Serial.println("safety feature: relay 1 off");
+    }
+  }
+  if (relay2Time > 0) {
+    if (millis() - relay2Time > SAFETY_TIME2) {
+      digitalWrite(relay2, LOW);
+      relay2Time = -1;
+      Serial.println("safety feature: relay 2 off");
+    }
+  }
+  if (relay3Time > 0) {
+    if (millis() - relay3Time > SAFETY_TIME3) {
+      digitalWrite(relay3, LOW);
+      relay3Time = -1;
+      Serial.println("safety feature: relay 3 off");
+    }
+  }
+  if (relay4Time > 0) {
+    if (millis() - relay4Time > SAFETY_TIME4) {
+      digitalWrite(relay4, LOW);
+      relay4Time = -1;
+      Serial.println("safety feature: relay 4 off");
+    }
+  }
 }
